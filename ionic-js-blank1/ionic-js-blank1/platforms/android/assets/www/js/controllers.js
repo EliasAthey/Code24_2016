42362fc0bd3dfa571a05c58fbf67325bb3724f24
Elias <Elias@DESKTOP-2T299NP>
2016-10-09 06:15:17 -0600

    Revert "Modals at start screen for login and signup. Main page format created"
    This reverts commit 82f9597355630c4a504d7037b2ac299ff4a7810f.

--- ionic-js-blank1/ionic-js-blank1/platforms/android/assets/www/js/controllers.js
+++ ionic-js-blank1/ionic-js-blank1/platforms/android/assets/www/js/controllers.js
@@ -1,18 +1,7 @@
 ﻿angular.module('WOTO.controllers', [])
 
-.controller('MainCtrl', function ($scope) { })
+.controller('MainCtrl', function ($scope) {})
 
-.controller('startCtrl', function ($scope, $ionicModal) {
-    $ionicModal.fromTemplateUrl('tabs/login.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-    }).then(function (modal) {
-        $scope.loginModal = modal;
-    });
-    $ionicModal.fromTemplateUrl('tabs/sign-up.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-    }).then(function (modal) {
-        $scope.supModal = modal;
-    });
-})
+.controller('signCtrl', function ($scope) { })
+
+.controller('supCtrl', function ($scope) { })
