82f9597355630c4a504d7037b2ac299ff4a7810f
Elias <Elias@DESKTOP-2T299NP>
2016-10-09 03:30:48 -0600

    Modals at start screen for login and signup. Main page format created

--- ionic-js-blank1/ionic-js-blank1/platforms/android/assets/www/js/controllers.js
+++ ionic-js-blank1/ionic-js-blank1/platforms/android/assets/www/js/controllers.js
@@ -1,7 +1,18 @@
 ﻿angular.module('WOTO.controllers', [])
 
-.controller('MainCtrl', function ($scope) {})
+.controller('MainCtrl', function ($scope) { })
 
-.controller('signCtrl', function ($scope) { })
-
-.controller('supCtrl', function ($scope) { })
+.controller('startCtrl', function ($scope, $ionicModal) {
+    $ionicModal.fromTemplateUrl('tabs/login.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.loginModal = modal;
+    });
+    $ionicModal.fromTemplateUrl('tabs/sign-up.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.supModal = modal;
+    });
+})
