b41143ed7711dd8d6b923769a01aed2928128117
Collin Feight <Collin Feight>
2016-11-01 16:50:23 -0600

    created backbone of job card

--- ionic-js-blank1/ionic-js-blank1/www/js/controllers.js
+++ ionic-js-blank1/ionic-js-blank1/www/js/controllers.js
@@ -26,14 +26,37 @@
  
     }
 
-    $scope.jobs = [
-        {
-            'title':'Lawn mowing needed', 'Pay': '15.00', '(x,y)': '.5 miles'
-        },
-        {
-            'title':'Driveway Clearing', 'Pay': '25.00', '(x,y)': '.7'
-        }
-    ]
+    $scope.items = [{
+      title: 'Job Title', 
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },{
+      title: 'Price',
+      
+    },{
+      title: 'Distance To',
+      
+    },{
+      title: '4',
+      
+    },{
+      title: '5',
+      
+  }];
+
+  /*
+   * if given group is the selected group, deselect it
+   * else, select the given group
+   */
+  $scope.toggleItem= function(item) {
+    if ($scope.isItemShown(item)) {
+      $scope.shownItem = null;
+    } else {
+      $scope.shownItem = item;
+    }
+  };
+  $scope.isItemShown = function(item) {
+    return $scope.shownItem === item;
+  };
 
 })
 
